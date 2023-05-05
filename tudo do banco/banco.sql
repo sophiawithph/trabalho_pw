cd4b22954e98f543a4f3e68e890b0aac3a53cff9
sophiawithph <sophiasgs11@gmail.com>
2023-05-05 10:43:05 -0300

    UPDATE BANCO SQL

--- tudo do banco/banco.sql
+++ tudo do banco/banco.sql
@@ -1,6 +1,15 @@
 
+CREATE DATABASE trabpw;
 
-
+CREATE TABLE `usuarios` (
+  `id` int(255) UNSIGNED NOT NULL,
+  `nome` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(45) CHARACTER SET utf8 NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `senha` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `admin` tinyint(4) NOT NULL,
+  `ativo` tinyint(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `trabpw`.`arquivos` (
@@ -11,3 +20,10 @@ CREATE TABLE IF NOT EXISTS `trabpw`.`arquivos` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`))
 ENGINE = InnoDB;
+
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+
+  // FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`))
