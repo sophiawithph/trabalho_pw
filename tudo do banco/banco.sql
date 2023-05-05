799cb41a9c3032604b1f0cb78489d9bd2e25ba9c
sophiawithph <sophiasgs11@gmail.com>
2023-05-05 11:09:39 -0300

    banco certo

--- tudo do banco/banco.sql
+++ tudo do banco/banco.sql
@@ -2,13 +2,14 @@
 CREATE DATABASE trabpw;
 
 CREATE TABLE `usuarios` (
-  `id` int(255) UNSIGNED NOT NULL,
+  `id` int UNSIGNED NOT NULL  AUTO_INCREMENT,
   `nome` varchar(255) CHARACTER SET utf8 NOT NULL,
   `email` varchar(45) CHARACTER SET utf8 NOT NULL,
   `username` varchar(255) CHARACTER SET utf8 NOT NULL,
   `senha` varchar(255) CHARACTER SET utf8 NOT NULL,
   `admin` tinyint(4) NOT NULL,
-  `ativo` tinyint(45) NOT NULL
+  `ativo` tinyint(45) NOT NULL,
+   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -21,9 +22,5 @@ CREATE TABLE IF NOT EXISTS `trabpw`.`arquivos` (
   FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`))
 ENGINE = InnoDB;
 
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-COMMIT;
 
 
-  // FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`))
